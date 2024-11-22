8a6a68166719c9644d87d293aa215051e483f22a
TheRealPad <pierre-alexandre.delgado-arevalo@epitech.eu>
2024-11-22 12:30:10 +0100

    update style for profil page

--- src/examples/Cards/InfoCards/ProfileInfoCard/index.js
+++ src/examples/Cards/InfoCards/ProfileInfoCard/index.js
@@ -33,7 +33,14 @@ import MDTypography from "components/MDTypography";
 import colors from "assets/theme/base/colors";
 import typography from "assets/theme/base/typography";
 
-function ProfileInfoCard({ title, description, info, social = null, action, shadow }) {
+function ProfileInfoCard({
+  title,
+  description,
+  info,
+  social = null,
+  action,
+  shadow,
+}) {
   const labels = [];
   const values = [];
   const { socialMediaColors } = colors;
@@ -43,7 +50,10 @@ function ProfileInfoCard({ title, description, info, social = null, action, shad
   Object.keys(info).forEach((el) => {
     if (el.match(/[A-Z\s]+/)) {
       const uppercaseLetter = Array.from(el).find((i) => i.match(/[A-Z]+/));
-      const newElement = el.replace(uppercaseLetter, ` ${uppercaseLetter.toLowerCase()}`);
+      const newElement = el.replace(
+        uppercaseLetter,
+        ` ${uppercaseLetter.toLowerCase()}`
+      );
 
       labels.push(newElement);
     } else {
@@ -57,7 +67,11 @@ function ProfileInfoCard({ title, description, info, social = null, action, shad
   // Render the card info items
   const renderItems = labels.map((label, key) => (
     <MDBox key={label} display="flex" py={1} pr={2}>
-      <MDTypography variant="button" fontWeight="bold" textTransform="capitalize">
+      <MDTypography
+        variant="button"
+        fontWeight="bold"
+        textTransform="capitalize"
+      >
         {label}: &nbsp;
       </MDTypography>
       <MDTypography variant="button" fontWeight="regular" color="text">
@@ -66,31 +80,33 @@ function ProfileInfoCard({ title, description, info, social = null, action, shad
     </MDBox>
   ));
 
-  // Render the card social media icons
-  // const renderSocial = social.map(({ link, icon, color }) => (
-  //   <MDBox
-  //     key={color}
-  //     component="a"
-  //     href={link}
-  //     target="_blank"
-  //     rel="noreferrer"
-  //     fontSize={size.lg}
-  //     color={socialMediaColors[color].main}
-  //     pr={1}
-  //     pl={0.5}
-  //     lineHeight={1}
-  //   >
-  //     {icon}
-    // </MDBox>
-  // ));
-
   return (
-    <Card sx={{ height: "100%", boxShadow: !shadow && "none" }}>
-      <MDBox display="flex" justifyContent="space-between" alignItems="center" pt={2} px={2}>
-        <MDTypography variant="h6" fontWeight="medium" textTransform="capitalize">
+    <Card
+      sx={{
+        height: "100%",
+        boxShadow: !shadow && "none",
+      }}
+    >
+      <MDBox
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        pt={2}
+        px={2}
+      >
+        <MDTypography
+          variant="h6"
+          fontWeight="medium"
+          textTransform="capitalize"
+        >
           {title}
         </MDTypography>
-        <MDTypography component={Link} to={action.route} variant="body2" color="secondary">
+        <MDTypography
+          component={Link}
+          to={action.route}
+          variant="body2"
+          color="secondary"
+        >
           <Tooltip title={action.tooltip} placement="top">
             <Icon>edit</Icon>
           </Tooltip>
--- src/layouts/profile/index.js
+++ src/layouts/profile/index.js
@@ -58,50 +58,59 @@ import { subscriptionList } from "./subscriptionsId";
 function Overview() {
   const [error, setError] = useState("");
   const [success, setSuccess] = useState("");
-  const {user, updateUser} = useUser();
-  const [subscriptionUser, setSubscriptionUser] = useState(new Subscription("0", "Free subcription", 0, new Date(), 0, "You don't have any subscription yet"));
+  const { user, updateUser } = useUser();
+  const [subscriptionUser, setSubscriptionUser] = useState(
+    new Subscription(
+      "0",
+      "Free subcription",
+      0,
+      new Date(),
+      0,
+      "You don't have any subscription yet"
+    )
+  );
 
   useEffect(() => {
     const getUserInfo = async () => {
       const userLocalStorage = localStorage.getItem("userData");
       if (userLocalStorage !== null) {
         const data = JSON.parse(userLocalStorage);
-        const updatedUser = {...user,
-          height : data.height,
-          subscription : data.subscription,
-          weight : data.weight,
-          birthDate : data.birthDate,
+        const updatedUser = {
+          ...user,
+          height: data.height,
+          subscription: data.subscription,
+          weight: data.weight,
+          birthDate: data.birthDate,
           createdAt: data.createdAt,
-          email : data.email,
-          firstName : data.firstName,
-          gender : data.gender,
-          lastName : data.lastName,
+          email: data.email,
+          firstName: data.firstName,
+          gender: data.gender,
+          lastName: data.lastName,
           password: data.password,
-          updatedAt : data.updatedAt,
-          stripeCustomerId : data.stripeCustomerId,
-          userId : data.data_id,
-          token : localStorage.getItem("authToken"),
-          tokens : data.tokens,
-          heartRate : data.heartRate,
-        }
+          updatedAt: data.updatedAt,
+          stripeCustomerId: data.stripeCustomerId,
+          userId: data.data_id,
+          token: localStorage.getItem("authToken"),
+          tokens: data.tokens,
+          heartRate: data.heartRate,
+        };
         updateUser(updatedUser);
         return;
       } else {
-          try {
-            const response = await axios.get(
-            "/api/api/user/account", {
-              headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${localStorage.getItem("authToken")}`
-              }
+        try {
+          const response = await axios.get("/api/api/user/account", {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${localStorage.getItem("authToken")}`,
             },
-          )
+          });
           if (response.status === 200) {
             console.log("Retrieve info user successful!");
           } else {
             console.error("Retrieve info user failed.");
           }
-          const updatedUser = {...user,
+          const updatedUser = {
+            ...user,
             height: 0,
             subscription: undefined,
             weight: 0,
@@ -115,18 +124,18 @@ function Overview() {
             updatedAt: "",
             stripeCustomerId: response.data.stripeCustomerId,
             userId: response.data._id,
-            tokens : response.data.tokens,
-            heartRate : response.data.heartRate,
-          }
-        updateUser(updatedUser);
-        console.log(updatedUser);
-      } catch (error) {
-        console.log(error);
+            tokens: response.data.tokens,
+            heartRate: response.data.heartRate,
+          };
+          updateUser(updatedUser);
+          console.log(updatedUser);
+        } catch (error) {
+          console.log(error);
+        }
       }
-      }
-      }
-      getUserInfo();
-      getSubscriptionInformation();
+    };
+    getUserInfo();
+    getSubscriptionInformation();
   }, []);
 
   const getSubscriptionInformation = async () => {
@@ -134,20 +143,32 @@ function Overview() {
       const response = await axios.get(
         "/api/api/subscriptions/getSubscription",
         {
-          headers : {
+          headers: {
             "Content-Type": "application/json",
-            "Authorization": `Bearer ${localStorage.getItem("authToken")}`
-          }
+            Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+          },
         }
       );
       if (response.data.message === "Subscription found") {
         const subscriptionId = response.data.data.plan.id;
-        const subscription = subscriptionList.find(sub => sub.subscriptionId === subscriptionId);
-        const startDate = new Date(+response.data.data.current_period_start * 1000);
+        const subscription = subscriptionList.find(
+          (sub) => sub.subscriptionId === subscriptionId
+        );
+        const startDate = new Date(
+          +response.data.data.current_period_start * 1000
+        );
         const endDate = new Date(+response.data.data.current_period_end * 1000);
-        const newSubscription = new Subscription(subscriptionId, subscription.name, subscription.price, startDate.toDateString(), subscription.length, subscription.description, endDate.toDateString());
+        const newSubscription = new Subscription(
+          subscriptionId,
+          subscription.name,
+          subscription.price,
+          startDate.toDateString(),
+          subscription.length,
+          subscription.description,
+          endDate.toDateString()
+        );
         setSubscriptionUser(newSubscription);
-        const updatedUser = {...user, subscription: newSubscription};
+        const updatedUser = { ...user, subscription: newSubscription };
         updateUser(updatedUser);
         localStorage.setItem("userData", JSON.stringify(updatedUser));
       } else {
@@ -156,15 +177,19 @@ function Overview() {
       }
     } catch (e) {
       console.log(e.response);
-      if (e.response && e.response.data && e.response.data.message === "Vous n'avez pas d'abonnement actif.") {
-        const updatedUser = {...user, subscription: subscriptionUser};
+      if (
+        e.response &&
+        e.response.data &&
+        e.response.data.message === "Vous n'avez pas d'abonnement actif."
+      ) {
+        const updatedUser = { ...user, subscription: subscriptionUser };
         updateUser(updatedUser);
         return;
       }
       console.error(e);
       console.error("Retrieve info for subscription failed");
     }
-  }
+  };
 
   return (
     <DashboardLayout>
@@ -177,32 +202,31 @@ function Overview() {
               <PlatformSettings />
             </Grid>
             <Grid item xs={12} md={6} xl={4} sx={{ display: "flex" }}>
-              <Divider orientation="vertical" sx={{ ml: 14, mr: 5 }} />
               <ProfileInfoCard
                 title="profile information"
                 description="You can see your profile information here."
                 info={{
                   fullName: user.firstName + " " + user.lastName,
                   email: user.email,
-                  Subscription : subscriptionUser.getSubscriptionInfo(),
-                  tokens : user.tokens,
+                  Subscription: subscriptionUser.getSubscriptionInfo(),
+                  tokens: user.tokens,
                 }}
                 // social={[
-                  // {
-                  //   link: "https://www.facebook.com/CreativeTim/",
-                  //   icon: <FacebookIcon />,
-                  //   color: "facebook",
-                  // },
-                  // {
-                  //   link: "https://twitter.com/creativetim",
-                  //   icon: <TwitterIcon />,
-                  //   color: "twitter",
-                  // },
-                  // {
-                  //   link: "https://www.instagram.com/creativetimofficial/",
-                  //   icon: <InstagramIcon />,
-                  //   color: "instagram",
-                  // },
+                // {
+                //   link: "https://www.facebook.com/CreativeTim/",
+                //   icon: <FacebookIcon />,
+                //   color: "facebook",
+                // },
+                // {
+                //   link: "https://twitter.com/creativetim",
+                //   icon: <TwitterIcon />,
+                //   color: "twitter",
+                // },
+                // {
+                //   link: "https://www.instagram.com/creativetimofficial/",
+                //   icon: <InstagramIcon />,
+                //   color: "instagram",
+                // },
                 // ]}
                 action={{ route: "", tooltip: "Edit Profile" }}
                 shadow={false}
@@ -211,7 +235,6 @@ function Overview() {
             </Grid>
           </Grid>
         </MDBox>
-
       </Header>
       <Footer />
     </DashboardLayout>
